d481a909f45b8a9bfc8c31ad098cba09d8a9649f
mle <maximilian@eul.cc>
2019-01-30 22:23:34 +0100

    test: mode -l

---
+++ test/10-test-line.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+. $(dirname "$0")/init.sh
+
+
+assertCmd "$HI -l NX9991 wxq NX9992 gld NX9993 < '$HERE/samples/default.txt'"
+wxq_gld="$ASSERTCMDOUTPUT"
+
+assertLineHighlighted "firstline" "$wxq_gld"  # contains wxq
+assertLineHighlighted "thirdline" "$wxq_gld"  # contains wxq AND gld
+assertLineHighlighted "seventhline" "$wxq_gld"  # contains gld
+assertLineNotHighlighted "secondline" "$wxq_gld"
+assertLineNotHighlighted "fifthline" "$wxq_gld"
+
+
+# no match:
+assertCmd "$HI -l NX9991 NX9992 < '$HERE/samples/default.txt'"
+assertHasNoHighlights "$ASSERTCMDOUTPUT"
+
+
+success
