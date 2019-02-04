31cb8ac6fc77db3871310ff8ed0ab562a4087e92
mle <maximilian@eul.cc>
2019-02-04 01:21:02 +0100

    fix type

--- src/main.c
+++ src/main.c
@@ -43,7 +43,7 @@ int main (int argc, char** argv) {
 
 	// Options:  ///////////////////////////////////////////////////////////
 
-	signed char c;
+	int c;
 	while ((c = getopt(argc, argv, "wplL:c:iIhV")) != -1)
 	switch (c) {
 		case 'w':  mode = MODE_WORDS; break;
